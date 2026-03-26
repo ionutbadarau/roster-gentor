8d4a22d5fb914cb3c611f3b089cd09d81cc04ed0
Ioan Badarau <ioan.badarau88@gmail.com>
2026-03-26 11:50:44 +0200

    fix client-reference-manifest issue on builds

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
-  return redirect("/grid");
+export default async function DashboardPage() {
+  redirect("/grid");
 }
